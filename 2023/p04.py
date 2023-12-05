46b3d1b6c9d78c5ccf6560affc35927a3e66755f
Jose Javier <3844846+JJGO@users.noreply.github.com>
2023-12-05 10:35:56 -0800

    Days 4 & 5

---
+++ 2023/p04.py
@@ -0,0 +1,50 @@
+import re
+
+
+def parses(input):
+    data = []
+    for line in input.strip().split("\n"):
+        card, nums = line.split(": ")
+        winning, have = nums.split(" | ")
+        winning = [int(i) for i in re.findall("\d+", winning)]
+        have = [int(i) for i in re.findall("\d+", have)]
+        data.append((winning, have))
+    return data
+
+
+def solve_a(data):
+    total = 0
+    for winning, have in data:
+        intersection = set(winning) & set(have)
+        if len(intersection) > 0:
+            total += 2 ** (len(intersection) - 1)
+    return total
+
+
+def solve_b(data):
+    cards = [1 for _ in data]
+    for i, (winning, have) in enumerate(data):
+        match = len(set(winning) & set(have))
+        for j in range(i + 1, i + 1 + match):
+            if j < len(cards):
+                cards[j] += cards[i]
+    return sum(cards)
+
+
+sample = parses("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""")
+
+
+if __name__ == "__main__":
+    from aocd.models import Puzzle
+
+    puzzle = Puzzle(year=2023, day=4)
+    data = parses(puzzle.input_data)
+    assert solve_a(sample) == 13
+    puzzle.answer_a = solve_a(data)
+    assert solve_b(sample) == 30
+    puzzle.answer_b = solve_b(data)
---
+++ 2023/p05.py
@@ -0,0 +1,111 @@
+import re
+
+
+def get_nums(line):
+    return [int(i) for i in re.findall("\d+", line)]
+
+
+def parses(input):
+    seeds, *maps = input.strip().split("\n\n")
+    seeds = get_nums(seeds)
+
+    parsed_maps = []
+    for map_ in maps:
+        _, *lines = map_.split("\n")
+        parsed_maps.append([get_nums(line) for line in lines])
+    return seeds, parsed_maps
+
+
+def solve_a(data):
+    vals, maps = data
+    maps = [sorted([(src, length, dst) for dst, src, length in map_]) for map_ in maps]
+
+    for map_ in maps:
+        new_vals = []
+        for val in vals:
+            for src, length, dst in map_:
+                if src <= val < src + length:
+                    new_vals.append(val - src + dst)
+                    break
+            else:
+                new_vals.append(val)
+        vals = new_vals
+    return min(vals)
+
+
+def solve_b(data):
+    vals, maps = data
+    vals = [(i, i + n) for i, n in sorted(zip(vals[::2], vals[1::2]))]
+    maps = [sorted([(src, src + m, dst) for dst, src, m in map_]) for map_ in maps]
+
+    for map_ in maps:
+        new_vals = []
+        for s1, e1 in vals:
+            for s2, e2, dst in map_:
+                if s2 <= s1 and e1 <= e2:
+                    # completely inside
+                    new_vals.append((dst + s1 - s2, dst + e1 - s2))
+                    s1 = e1
+                    continue
+                elif s2 <= s1 < e2:
+                    # partially inside
+                    new_vals.append((dst + s1 - s2, dst + e2 - s2))
+                    s1 = e2
+                    continue
+                elif s1 <= s2 < e1:
+                    # partially outside
+                    new_vals.append((s1, s2))
+                    s1 = s2
+                    continue
+            if e1 - s1 > 0:
+                new_vals.append((s1, e1))
+        vals = sorted(new_vals)
+    return min(vals)[0]
+
+
+sample = parses(
+    """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+)
+
+
+if __name__ == "__main__":
+    from aocd.models import Puzzle
+
+    puzzle = Puzzle(year=2023, day=5)
+    data = parses(puzzle.input_data)
+    assert solve_a(sample) == 35
+    puzzle.answer_a = solve_a(data)
+    assert solve_b(sample) == 46
+    puzzle.answer_b = solve_b(data)
